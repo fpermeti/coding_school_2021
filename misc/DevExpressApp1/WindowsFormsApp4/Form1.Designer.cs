9a191076ec1c308b366405e6e555c0449f44fb4d
Frantsesko Permeti <frantseskopermeti@gmail.com>
2021-06-09 15:42:30 +0300

    Experimenting with DevExpress

---
+++ misc/DevExpressApp1/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace WindowsFormsApp4 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.ribbonPage2 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.listBoxControl1 = new DevExpress.XtraEditors.ListBoxControl();
+            this.tabFormControl1 = new DevExpress.XtraBars.TabFormControl();
+            this.tabFormPage1 = new DevExpress.XtraBars.TabFormPage();
+            this.tabFormContentContainer1 = new DevExpress.XtraBars.TabFormContentContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabFormControl1)).BeginInit();
+            this.tabFormContentContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(170, 65);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(288, 22);
+            this.textEdit1.TabIndex = 0;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(520, 61);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(82, 29);
+            this.simpleButton1.TabIndex = 1;
+            this.simpleButton1.Text = "Add";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // ribbonPage2
+            // 
+            this.ribbonPage2.Name = "ribbonPage2";
+            this.ribbonPage2.Text = "ribbonPage2";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(67, 68);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(71, 16);
+            this.labelControl1.TabIndex = 4;
+            this.labelControl1.Text = "Enter name:";
+            // 
+            // listBoxControl1
+            // 
+            this.listBoxControl1.Location = new System.Drawing.Point(170, 153);
+            this.listBoxControl1.Name = "listBoxControl1";
+            this.listBoxControl1.Size = new System.Drawing.Size(288, 208);
+            this.listBoxControl1.TabIndex = 5;
+            this.listBoxControl1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBoxControl1_MouseDoubleClick);
+            // 
+            // tabFormControl1
+            // 
+            this.tabFormControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabFormControl1.Name = "tabFormControl1";
+            this.tabFormControl1.Pages.Add(this.tabFormPage1);
+            this.tabFormControl1.SelectedPage = this.tabFormPage1;
+            this.tabFormControl1.Size = new System.Drawing.Size(700, 88);
+            this.tabFormControl1.TabForm = this;
+            this.tabFormControl1.TabIndex = 6;
+            this.tabFormControl1.TabStop = false;
+            // 
+            // tabFormPage1
+            // 
+            this.tabFormPage1.ContentContainer = this.tabFormContentContainer1;
+            this.tabFormPage1.Name = "tabFormPage1";
+            this.tabFormPage1.ShowCloseButton = DevExpress.Utils.DefaultBoolean.False;
+            this.tabFormPage1.Text = "Page 0";
+            // 
+            // tabFormContentContainer1
+            // 
+            this.tabFormContentContainer1.Controls.Add(this.listBoxControl1);
+            this.tabFormContentContainer1.Controls.Add(this.labelControl1);
+            this.tabFormContentContainer1.Controls.Add(this.simpleButton1);
+            this.tabFormContentContainer1.Controls.Add(this.textEdit1);
+            this.tabFormContentContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabFormContentContainer1.Location = new System.Drawing.Point(0, 88);
+            this.tabFormContentContainer1.Name = "tabFormContentContainer1";
+            this.tabFormContentContainer1.Size = new System.Drawing.Size(700, 441);
+            this.tabFormContentContainer1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 529);
+            this.Controls.Add(this.tabFormContentContainer1);
+            this.Controls.Add(this.tabFormControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TabFormControl = this.tabFormControl1;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabFormControl1)).EndInit();
+            this.tabFormContentContainer1.ResumeLayout(false);
+            this.tabFormContentContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage2;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.ListBoxControl listBoxControl1;
+        private DevExpress.XtraBars.TabFormControl tabFormControl1;
+        private DevExpress.XtraBars.TabFormPage tabFormPage1;
+        private DevExpress.XtraBars.TabFormContentContainer tabFormContentContainer1;
+    }
+}
+
---
+++ misc/DevExpressApp1/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4 {
+    public partial class Form1 : DevExpress.XtraBars.TabForm {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e) {
+
+            string message1 = "Only alphabet characters are allowed.\n\n";
+
+            string message2 = "Numbers, symbols and spacing are not allowed.";
+
+            Regex onlyAlphabetLetters = new Regex(@"^[a-zA-Z]+$");
+
+            MatchCollection matches = onlyAlphabetLetters.Matches(textEdit1.Text);
+
+            if (matches.Count > 0) {
+
+                int nameLength = textEdit1.Text.Length;
+
+                string firstLetterOfName = textEdit1.Text.Substring(0, 1).ToUpper();
+
+                string nameWithoutFirstLetter = textEdit1.Text.Substring(1, nameLength - firstLetterOfName.Length).ToLower();
+
+                string processedName = string.Format("{0}{1}", firstLetterOfName, nameWithoutFirstLetter);
+
+                if (!listBoxControl1.Items.Contains(processedName)) {
+
+                    listBoxControl1.Items.Add(processedName);
+                }
+                else {
+
+                    MessageBox.Show("The name you are trying to add already exists.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+            }
+            else {
+
+                MessageBox.Show(message1 + message2, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+        }
+
+        private void listBoxControl1_MouseDoubleClick(object sender, MouseEventArgs e) {
+
+            int index = this.listBoxControl1.IndexFromPoint(e.Location);
+
+            if (index != -1) {
+
+                MessageBox.Show(listBoxControl1.SelectedItem.ToString(), "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+    }
+}

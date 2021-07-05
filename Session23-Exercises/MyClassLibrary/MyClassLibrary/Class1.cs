f85f07989f1244e6ea218154e59395e880a5e374
Frantsesko Permeti <frantseskopermeti@gmail.com>
2021-07-05 13:25:54 +0300

    create IFibonacciCalculator interface and FibonacciCalculator class

--- Session23-Exercises/MyClassLibrary/MyClassLibrary/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace MyClassLibrary {
-    public class Class1 {
-    }
-}
---
+++ Session23-Exercises/MyClassLibrary/MyClassLibrary/FibonacciCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary {
+   public class FibonacciCalculator : IFibonacciCalculator {
+         public long FibonacciSeries(long n) {
+
+            if (n<0) {
+                throw new ArgumentException("Invalid input", nameof(n));
+            }
+
+           else if (n==0) {
+                return 0;
+            }
+            else if (n==1) {
+                return 1;
+            }
+            else if (90 > n && n > 1) {
+                return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+            }
+            else {                  //if (n >= 90) 
+                throw new ArgumentException("Fibonacci result number too big", nameof(n));
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
